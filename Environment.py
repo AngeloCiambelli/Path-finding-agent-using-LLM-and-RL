8c42d30e0fe3a4520f085e0cd6effa7e2777f7b2
simon <simon.bure@mailo.com>
2025-01-22 16:30:34 +0100

    Adding a position type which depends on the environment + tests

--- Environment.py
+++ Environment.py
@@ -4,14 +4,23 @@ from Position import Position
 class Environment:
     size : int
     state_values: np.ndarray
+    terminal_state: Position
 
     def __init__(self, size: int, terminal_state: Position):
         self.size = size
         self.state_values = - np.random.random((size, size))
+        self.terminal_state = terminal_state
         self.state_values[terminal_state.x, terminal_state.y] = 0
 
     def __repr__(self):
         return str(self.state_values)
+
+    def compute_state_values_with_policy(self, policy: tuple):
+        pass
+
+    def compute_state_value_at_position(self, position: Position):
+        pass
+
 
     def get_possible_actions_at_position(self, position: Position) -> list:
         # Corners
@@ -38,3 +47,32 @@ class Environment:
             possible_actions = ['up', 'down', 'right', 'left']
 
         return possible_actions
+
+
+if __name__ == '__main__':
+    size = 4
+    pos_bottom_left_corner = Position(0, 0)
+    pos_bottom_right_corner = Position(size - 1, 0)
+    pos_top_left_corner = Position(0, size - 1)
+    pos_top_right_corner = Position(size - 1, size - 1)
+
+    env = Environment(size=size, terminal_state=pos_bottom_left_corner)
+
+    # Testing getting the type of Position in a given environment
+    assert pos_bottom_left_corner.get_type_according_to_environment(env) == 'corner'
+    assert pos_bottom_right_corner.get_type_according_to_environment(env) == 'corner'
+    assert pos_top_left_corner.get_type_according_to_environment(env) == 'corner'
+    assert pos_top_right_corner.get_type_according_to_environment(env) == 'corner'
+
+    pos_left_border = Position(0, np.random.randint(1, size - 2))
+    pos_right_border = Position(size - 1, np.random.randint(1, size - 2))
+    pos_bottom_border = Position(np.random.randint(1, size - 2), 0)
+    pos_top_border = Position(np.random.randint(1, size - 2), size - 1)
+
+    assert pos_left_border.get_type_according_to_environment(env) == 'border'
+    assert pos_right_border.get_type_according_to_environment(env) == 'border'
+    assert pos_bottom_border.get_type_according_to_environment(env) == 'border'
+    assert pos_top_border.get_type_according_to_environment(env) == 'border'
+
+    random_pos = Position(np.random.randint(1, size - 2), np.random.randint(1, size - 2))
+    assert random_pos.get_type_according_to_environment(env) == 'free'
--- Position.py
+++ Position.py
@@ -3,7 +3,33 @@
 class Position:
     x: int
     y: int
+    type: str
 
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    def __repr__(self):
+        return f"Position({self.x}, {self.y})"
+
+    def get_type_according_to_environment(self, env) -> str:
+        is_bottom_left_corner = self.x == 0 and self.y == 0
+        is_bottom_right_corner = self.x == env.size - 1 and self.y == 0
+        is_top_left_corner = self.x == 0 and self.y == env.size - 1
+        is_top_right_corner = self.x == env.size - 1 and self.y == env.size - 1
+
+        is_left_border = self.x == 0 and 0 < self.y < env.size - 1
+        is_bottom_border = self.y == 0 < self.x < env.size - 1
+        is_right_border = self.x == env.size - 1 and 0 < self.y < env.size - 1
+        is_top_border = self.y == env.size - 1 and 0 < self.x < env.size - 1
+
+        if is_bottom_left_corner or is_bottom_right_corner or is_top_left_corner or is_top_right_corner:
+            type = 'corner'
+
+        elif is_left_border or is_right_border or is_bottom_border or is_top_border:
+            type = 'border'
+
+        else:
+            type = 'free'
+
+        return type
