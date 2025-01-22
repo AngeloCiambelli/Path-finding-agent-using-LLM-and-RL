39f393d8039f0a5000668abaaed50c4e458d246c
simon <simon.bure@mailo.com>
2025-01-22 22:16:55 +0100

    Removed the qualitative nature of the position because of too much complexity. Creation and test of the algorithm to compute and update the state values according to a uniform policy

--- Environment.py
+++ Environment.py
@@ -3,71 +3,123 @@ from Position import Position
 
 class Environment:
     size : int
+    possible_actions: list
     state_values: np.ndarray
+    action_values: list
     terminal_state: Position
+    policy: list
+    reward : float
+    discount : float
 
-    def __init__(self, size: int, terminal_state: Position):
+    def __init__(self, size: int, terminal_state: Position, reward: float = -1., discount: float = 1.):
         self.size = size
-        self.state_values = - np.random.random((size, size))
+
+        self.possible_actions = [Position(0, -1), Position(0, 1), Position(-1, 0), Position(1, 0)]
+
         self.terminal_state = terminal_state
+        self.state_values = np.random.random((size, size))
         self.state_values[terminal_state.x, terminal_state.y] = 0
+
+        self.action_values = np.random.random((size, size, 4))  # four possible maximum action for each state
+
+        self.create_policy()
+
+        self.reward = reward
+        self.discount = discount
 
     def __repr__(self):
         return str(self.state_values)
 
-    def compute_state_values_with_policy(self, policy: tuple):
-        pass
+    def create_policy(self):
+        # policy is of dimension 4 * size * size, for each state of the environment, it stores the four probabilities
+        # associated to each of its four possible action :
+        # 0/3 array -> moving left action ; 1/3 -> moving up ; 2/3 -> moving right ; 3/3 -> moving down
+        self.policy = 0.25 * np.ones((4, self.size, self.size))
+        # There is a zero probability of stepping out of the environment
+        self.policy[0, :, 0] = 0.
+        self.policy[1, 0, :] = 0.
+        self.policy[2, :, -1] = 0.
+        self.policy[3, -1, :] = 0.
 
-    def compute_state_value_at_position(self, position: Position):
-        pass
+        self.policy /= np.sum(self.policy, axis=0, keepdims=True)
 
-
-    def get_possible_actions_at_position(self, position: Position) -> list:
-        # Corners
-        if position.x == 0 and position.y == 0:
-            possible_actions = ['down', 'right']
-        elif position.x == 0 and position.y == self.size - 1:
-            possible_actions = ['down', 'left']
-        elif position.x == self.size - 1 and position.y == 0:
-            possible_actions = ['up', 'right']
-        elif position.x == self.size - 1 and position.y == self.size - 1:
-            possible_actions = ['up', 'left']
-
-        # Borders
-        elif position.x == 0 and 0 < position.y < self.size - 1:
-            possible_actions = ['up', 'down', 'left']
-        elif 0 < position.x < self.size - 1 and position.y == 0:
-            possible_actions = ['down', 'right', 'left']
-        elif position.x == self.size - 1 and 0 < position.y < self.size - 1:
-            possible_actions = ['up', 'down', 'left']
-        elif 0 < position.x < self.size - 1 and position.y == self.size - 1:
-            possible_actions = ['up', 'right', 'left']
-
+    def get_state_values_at_position(self, position: Position) -> float:
+        if position.x < 0 or position.y < 0 or position.x > self.size - 1 or position.y > self.size - 1:
+            return 0.0
         else:
-            possible_actions = ['up', 'down', 'right', 'left']
+            return self.state_values[position.x, position.y]
 
-        return possible_actions
+    def get_actions_probabilities_at_position(self, position: Position) -> list:
+        return self.policy[:, position.y, position.x]
+
+    def update_state_values(self):
+        variation = 0
+        for i in range(self.size):
+            for j in range(self.size):
+                # Don't parse the terminal state
+                if i != self.terminal_state.x or j != self.terminal_state.y:
+                    new_state_value = self.compute_new_state_value_at_position(Position(i, j))
+                    variation += np.abs(new_state_value - self.state_values[i, j])
+                    self.state_values[i, j] = new_state_value
+
+        return variation / (self.size ** 2)
+
+    def compute_new_state_value_at_position(self, position: Position) -> float:
+        possible_future_positions = [position + Position(-1, 0), position + Position(0, -1),
+                                     position + Position(1, 0), position + Position(0, 1)]
+        action_probabilities = self.get_actions_probabilities_at_position(position)
+
+        new_state_value = 0
+        for pos, action_proba in zip(possible_future_positions, action_probabilities):
+            new_state_value += action_proba * (self.reward + self.discount * self.get_state_values_at_position(pos))
+
+        return new_state_value
+
+    def update_action_values(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if i != self.terminal_state.x and j != self.terminal_state.y:
+                    for a, k in enumerate(self.possible_actions):
+                        self.action_values[i, j, k] = self.compute_action_value_at_position_for_action(Position(i, j), a)
+
+    def compute_action_value_at_position_for_action(self, position: Position, action: Position) -> float:
+        return self.reward + self.discount * self.get_state_values_at_position(position + action)
 
 
 if __name__ == '__main__':
+    np.random.seed(seed=1)
+
     size = 4
     pos_bottom_left_corner = Position(0, 0)
     pos_bottom_right_corner = Position(size - 1, 0)
     pos_top_left_corner = Position(0, size - 1)
     pos_top_right_corner = Position(size - 1, size - 1)
 
+    pos_left_border = Position(0, np.random.randint(1, size - 2))
+    pos_right_border = Position(size - 1, np.random.randint(1, size - 2))
+    pos_bottom_border = Position(np.random.randint(1, size - 2), 0)
+    pos_top_border = Position(np.random.randint(1, size - 2), size - 1)
+
     env = Environment(size=size, terminal_state=pos_bottom_left_corner)
+    print(env)
+    print(env.policy[:, 0, 3])
+
+    print(env.compute_new_state_value_at_position(pos_bottom_right_corner))
+    print(env.compute_new_state_value_at_position(pos_top_left_corner))
+
+    env.update_state_values()
+    print(env)
+
+    mean_variation = env.update_state_values()
+    while mean_variation > 1e-2:
+        mean_variation = env.update_state_values()
+        print(env)
 
     # Testing getting the type of Position in a given environment
     assert pos_bottom_left_corner.get_nature_according_to_environment(env) == 'bottom left corner'
     assert pos_bottom_right_corner.get_nature_according_to_environment(env) == 'bottom right corner'
     assert pos_top_left_corner.get_nature_according_to_environment(env) == 'top left corner'
     assert pos_top_right_corner.get_nature_according_to_environment(env) == 'top right corner'
-
-    pos_left_border = Position(0, np.random.randint(1, size - 2))
-    pos_right_border = Position(size - 1, np.random.randint(1, size - 2))
-    pos_bottom_border = Position(np.random.randint(1, size - 2), 0)
-    pos_top_border = Position(np.random.randint(1, size - 2), size - 1)
 
     assert pos_left_border.get_nature_according_to_environment(env) == 'left border'
     assert pos_right_border.get_nature_according_to_environment(env) == 'right border'
--- Position.py
+++ Position.py
@@ -12,36 +12,5 @@ class Position:
     def __repr__(self):
         return f"Position({self.x}, {self.y})"
 
-    def get_nature_according_to_environment(self, env) -> str:
-        is_bottom_left_corner = self.x == 0 and self.y == 0
-        is_bottom_right_corner = self.x == env.size - 1 and self.y == 0
-        is_top_left_corner = self.x == 0 and self.y == env.size - 1
-        is_top_right_corner = self.x == env.size - 1 and self.y == env.size - 1
-
-        is_left_border = self.x == 0 and 0 < self.y < env.size - 1
-        is_bottom_border = self.y == 0 < self.x < env.size - 1
-        is_right_border = self.x == env.size - 1 and 0 < self.y < env.size - 1
-        is_top_border = self.y == env.size - 1 and 0 < self.x < env.size - 1
-
-        if is_bottom_left_corner:
-            nature = 'bottom left corner'
-        elif is_bottom_right_corner:
-            nature = 'bottom right corner'
-        elif is_top_left_corner:
-            nature = 'top left corner'
-        elif is_top_right_corner:
-            nature = 'top right corner'
-
-        elif is_left_border:
-            nature = 'left border'
-        elif is_right_border:
-            nature = 'right border'
-        elif is_bottom_border:
-            nature = 'bottom border'
-        elif is_top_border:
-            nature = 'top border'
-
-        else:
-            nature = 'free'
-
-        return nature
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y)
